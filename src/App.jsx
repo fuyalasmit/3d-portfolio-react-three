bcc5834f3b866013f6134c7a2f5f7c7e85a88ad9
fuyal.asmit <asmitphuyal4040@gmail.com>
2024-12-30 09:31:05 +0545

    initializations

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+
+    </div>
+      <Navbar/>
+    </BrowserRouter>
+  )
+}
+
+export default App
